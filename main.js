1272184412e116bed6c0e10c02a450bffcc126de
‘YOUR_PREFERRED_USERNAME’Clint <clintjordan5@gmail.com>
2020-08-19 16:19:14 -0400

    made index and mains with outline to test

---
+++ main.js
@@ -0,0 +1,9 @@
+// will use this function for addition and subtraction?
+function sum(arrayNumbers) {
+    let num = 0
+    for (let check of arrayNumbers) {
+        num += check
+    }
+    return num
+}
+// create multiplication function
