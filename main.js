aaa377e57717746a2272fd10b041f8eea68f1f29
‘YOUR_PREFERRED_USERNAME’Clint <clintjordan5@gmail.com>
2020-08-20 23:09:53 -0400

    html and css updated classes and format. js console error on event listener.

--- main.js
+++ main.js
@@ -1,9 +1,63 @@
-// will use this function for addition and subtraction?
-function sum(arrayNumbers) {
-    let num = 0
-    for (let check of arrayNumbers) {
-        num += check
-    }
-    return num
-}
-// create multiplication function
+let number0 = document.querySelector (".number0")
+number0.addeventListener ("click", function () {
+    display.textcontent +- "0"
+    console.log (0)
+})
+
+let number1 = document.querySelector (".number1")
+number1.addeventListener ("click", function () {
+    display.textcontent +- "1"
+    console.log (1)
+})
+
+let number2 = document.querySelector (".number2")
+number2.addeventListener ("click", function () {
+    display.textcontent +- "2"
+    console.log (2)
+})
+
+let number3 = document.querySelector (".number3")
+number3.addeventListener ("click", function () {
+    display.textcontent +- "3"
+    console.log (3)
+})
+
+let number4 = document.querySelector (".number4")
+number4.addeventListener ("click", function () {
+    display.textcontent +- "4"
+    console.log (4)
+})
+
+let number5 = document.querySelector (".number5")
+number5.addeventListener ("click", function () {
+    display.textcontent +- "5"
+    console.log (6)
+})
+
+let number6 = document.querySelector (".number6")
+number6.addeventListener ("click", function () {
+    display.textcontent +- "6"
+    console.log (6)
+})
+
+let number7 = document.querySelector (".number7")
+number7.addeventListener ("click", function () {
+    display.textcontent +- "7"
+    console.log (7)
+})
+
+let number8 = document.querySelector (".number8")
+number8.addeventListener ("click", function () {
+    display.textcontent +- "8"
+    console.log (8)
+})
+
+let number9 = document.querySelector (".number9")
+number1.addeventListener ("click", function () {
+    display.textcontent +- "9"
+    console.log (9)
+})
+
+// note - wrote out queryselector and event listener for 1-9. 
+// looking at console for index.html the error is 
+// Uncaught TypeError: Cannot read property 'addeventListener' of null at main.js:2
